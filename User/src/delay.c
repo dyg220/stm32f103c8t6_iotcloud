4ccf9d840c645d6294395f7d1c9f55289a9fbe01
dyg220 <1503639251@qq.com>
2025-06-30 12:00:00 +0800

    add

--- User/src/delay.c
+++ User/src/delay.c
@@ -10,17 +10,13 @@
 
 void delay_us(uint32_t xus)
 {
-	while (xus--)
-	{
-		__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); __ASM("nop"); \
-			__ASM("nop"); __ASM("nop");
-	}
+	SysTick->CTRL &= ~(1 << 2);//外部参考时钟
+	SysTick->LOAD = 9 * xus - 1;//重装载值
+	SysTick->VAL = 0;//清除计数值
+	SysTick->CTRL &= ~(1 << 1);//不使能中断
+	SysTick->CTRL |= (1 << 0);//启动计数器
+	while (!(SysTick->CTRL & (1 << 16)));//等待计数完成
+	SysTick->CTRL &= ~(1 << 0);//关闭计数器
 }
 
 void delay_ms(uint32_t xms)
--- User/src/main.c
+++ User/src/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	u8 key_num = 0;
+	//u8 key_num = 0;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4); // 关系到配置抢占和响应数字的范围
 	LED_Init();
 	Key_Init();
@@ -15,22 +15,23 @@ int main()
 	//RGB_Control(0, 0, 0xff);
 	while (1)
 	{
-		key_num = Key_Scan();
-		if (key_num == key1_click)
-		{
-			//DCMotor_SetSpeed(100);
-		}
-		else if (key_num == key2_click)
-		{
-			//DCMotor_SetSpeed(-100);
-		}
-		else if (key_num == key3_click)
-		{
-			//DCMotor_SetSpeed(0);
-		}
+		delay_ms(500);
+		printf("1!\r\n");
+		//key_num = Key_Scan();
+		//if (key_num == key1_click)
+		//{
+		//	//DCMotor_SetSpeed(100);
+		//}
+		//else if (key_num == key2_click)
+		//{
+		//	//DCMotor_SetSpeed(-100);
+		//}
+		//else if (key_num == key3_click)
+		//{
+		//	//DCMotor_SetSpeed(0);
+		//}
 		RECV_USART1(); // 接收USART1数据
 	}
 	return 0;
 }
-
 
