5513b79987dc1e862ea0e9530f44826f358b44fe
dyg220 <1503639251@qq.com>
2025-07-03 12:01:56 +0800

    add .

--- Hardwear/OLED/OLED_Data.h
+++ Hardwear/OLED/OLED_Data.h
@@ -3,10 +3,23 @@
 
 #include <stdint.h>
 
+/*字符集定义*/
+/*以下两个宏定义只可解除其中一个的注释*/
+//#define OLED_CHARSET_UTF8			//定义字符集为UTF8
+#define OLED_CHARSET_GB2312		//定义字符集为GB2312
+
 /*字模基本单元*/
 typedef struct
 {
+
+#ifdef OLED_CHARSET_UTF8			//定义字符集为UTF8
+	char Index[5];					//汉字索引，空间为5字节
+#endif
+
+#ifdef OLED_CHARSET_GB2312			//定义字符集为GB2312
 	char Index[3];					//汉字索引，空间为3字节
+#endif
+
 	uint8_t Data[32];				//字模数据
 } ChineseCell_t;
 
@@ -23,4 +36,5 @@ extern const uint8_t Diode[];
 //...
 
 #endif
+
 
--- User/src/main.c
+++ User/src/main.c
@@ -5,63 +5,64 @@ u8 Humi = 0;
 
 int main()
 {
-	// u8 key_num = 0;
-	// u8 ret = 0;
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4); // 关系到配置抢占和响应数字的范围
-	LED_Init();
-	Key_Init();
-	Relay_Init();
-	USART1_Config(115200);
-	// dcMotor_Config();
-	// StepMotor_Init();
-	// USART1_SendStr((u8*)"Hello, USART1!\r\n");
-	RGB_Init();
-	DHT11_Init();
-	Infrared_Init();
-	OLED_Init();
-	OLED_setCursor(20, 0);
-	printf("Reset!!!\r\n");
-	// RGB_Control(0, 0, 0xff);
-	while (1)
-	{
-		// StepMotor_Control(100, 1);
-		/*ret = DHT11_GetTempHumi(&Temp, &Humi);
-		if (ret)
-		{
-			printf("ret = %d\r\n", ret);
-		}
-		else
-		{
-			printf("Temp：%.1f℃   Humi：%d%%\r\n", Temp, Humi);
-		}
-		delay_ms(2000);*/
+    // u8 key_num = 0;
+    // u8 ret = 0;
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4); // 关系到配置抢占和响应数字的范围
+    LED_Init();
+    Key_Init();
+    Relay_Init();
+    USART1_Config(115200);
+    // dcMotor_Config();
+    // StepMotor_Init();
+    // USART1_SendStr((u8*)"Hello, USART1!\r\n");
+    RGB_Init();
+    DHT11_Init();
+    Infrared_Init();
+    OLED_Init();
+    OLED_ShowNum(0, 0, 12345, 5, OLED_8X16);
+    OLED_Update();
+    printf("Reset!!!\r\n");
+    // RGB_Control(0, 0, 0xff);
+    while (1)
+    {
+        // StepMotor_Control(100, 1);
+        /*ret = DHT11_GetTempHumi(&Temp, &Humi);
+        if (ret)
+        {
+            printf("ret = %d\r\n", ret);
+        }
+        else
+        {
+            printf("Temp：%.1f℃   Humi：%d%%\r\n", Temp, Humi);
+        }
+        delay_ms(2000);*/
 
-		if (Infrared_info_t.CaptureOver == 1) // 采集结束
-		{
-			Infrared_info_t.CaptureOver = 0;
-			printf("捕获的边沿数：%d\r\n", Infrared_info_t.EdgeCnt);
-			for (u8 i = 0; i < Infrared_info_t.EdgeCnt; i++)
-			{
-				printf("%d ", Infrared_info_t.TimeCnt[i]);
-			}
-			printf("\r\n");
-			Infrared_AnalysisNECBit(1);
-		}
+        if (Infrared_info_t.CaptureOver == 1) // 采集结束
+        {
+            Infrared_info_t.CaptureOver = 0;
+            printf("捕获的边沿数：%d\r\n", Infrared_info_t.EdgeCnt);
+            for (u8 i = 0; i < Infrared_info_t.EdgeCnt; i++)
+            {
+                printf("%d ", Infrared_info_t.TimeCnt[i]);
+            }
+            printf("\r\n");
+            Infrared_AnalysisNECBit(1);
+        }
 
-		// key_num = Key_Scan();
-		// if (key_num == key1_click)
-		//{
-		//	//DCMotor_SetSpeed(100);
-		// }
-		// else if (key_num == key2_click)
-		//{
-		//	//DCMotor_SetSpeed(-100);
-		// }
-		// else if (key_num == key3_click)
-		//{
-		//	//DCMotor_SetSpeed(0);
-		// }
-		RECV_USART1(); // 接收USART1数据
-	}
-	return 0;
+        // key_num = Key_Scan();
+        // if (key_num == key1_click)
+        //{
+        //	//DCMotor_SetSpeed(100);
+        // }
+        // else if (key_num == key2_click)
+        //{
+        //	//DCMotor_SetSpeed(-100);
+        // }
+        // else if (key_num == key3_click)
+        //{
+        //	//DCMotor_SetSpeed(0);
+        // }
+        RECV_USART1(); // 接收USART1数据
+    }
+    return 0;
 }
